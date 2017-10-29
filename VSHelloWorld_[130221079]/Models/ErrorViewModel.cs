b888fab758e0ac321fb161c36a511efd21194143
inesbatistarodrigues <32805145+inesbatistarodrigues@users.noreply.github.com>
2017-10-29 17:17:43 +0000

    Add project files.

---
+++ VSHelloWorld_[130221079]/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VSHelloWorld__130221079_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
